16092cf80a7166273884994e15b27864a750c02e
Bernardo Ayala <bernardoayala2@gmail.com>
2020-04-13 08:23:52 -0400

    typo fixed in header

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -4,7 +4,7 @@ const Header = ()=> {
       <div class="Header-logo">
         <h1>
           <a href="/">
-            100tifi.com
+            100tifi.co
           </a>
         </h1>
       </div>
